8e2058de6a50893c4e9e6e7bacaa62404f63778f
rochi88 <raisul.me@gmail.com>
2024-08-20 09:04:27 +0600

    new skach

---
+++ arduino_freeRTOS/dht22_webserver/dht22_webserver.ino
@@ -0,0 +1,109 @@
+#define ESP32_RTOS // Uncomment this line if you want to use the code with freertos only on the ESP32
+
+#include <WiFi.h>
+#include <WebServer.h>
+#include "DHT.h"
+#include "credentials.h"
+
+#define DHTPIN 10     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22 // DHT 22 (AM2302), AM2321
+
+WebServer server(80); // Create a web server object that listens for HTTP requests on port 80
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float Temperature;
+float Humidity;
+
+#define LED_PIN 8
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(F("DHTxx test with web server!"));
+  dht.begin();
+  pinMode(LED_PIN, OUTPUT);
+
+  Serial.println("Connecting to wifi");
+  WiFi.begin(mySSID, myPASSWORD); // Connect to Wi-Fi network
+
+  // Wait until the device is connected to Wi-Fi
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP()); // Print the IP address
+
+  server.on("/", handle_OnConnect);   // Define handling function for root URL
+  server.onNotFound(handle_NotFound); // Define handling function for 404 (Not Found)
+
+  server.begin(); // Start the HTTP server
+  Serial.println("HTTP server started");
+}
+
+void loop()
+{
+#ifdef defined(ESP32_RTOS) && defined(ESP32)
+#else
+  server.handleClient(); // Handle client requests
+#endif
+}
+
+// Handle connection to the root URL
+void handle_OnConnect()
+{
+  Temperature = dht.readTemperature(); // Get temperature value
+  Humidity = dht.readHumidity();       // Get humidity value
+
+  // Check if any reading failed and send "N/A" if so
+  if (isnan(Temperature) || isnan(Humidity))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    server.send(200, "text/html", SendHTML("N/A", "N/A"));
+  }
+  else
+  {
+    Serial.print("Temperature: ");
+    Serial.println(Temperature);
+    Serial.print("Humidity: ");
+    Serial.println(Humidity);
+    server.send(200, "text/html", SendHTML(String(Temperature), String(Humidity))); // Send the HTML page with the values
+  }
+}
+
+// Handle not found URL
+void handle_NotFound()
+{
+  server.send(404, "text/plain", "Not found");
+}
+
+// Generate the HTML content to display
+String SendHTML(String Temperaturestat, String Humiditystat)
+{
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>ESP32 Temperature & Humidity</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<div id=\"webpage\">\n";
+  ptr += "<h1>ESP32 Temperature & Humidity</h1>\n";
+
+  ptr += "<p>Temperature: ";
+  ptr += Temperaturestat;
+  ptr += " &deg;C</p>";
+  ptr += "<p>Humidity: ";
+  ptr += Humiditystat;
+  ptr += "%</p>";
+
+  ptr += "</div>\n";
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+  return ptr;
+}
